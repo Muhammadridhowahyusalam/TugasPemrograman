663bfcf590dec1787b86c002c2b7ee050714c236
Muhammadridhowahyusalam <117148915+Muhammadridhowahyusalam@users.noreply.github.com>
2023-10-02 11:36:14 +0700

    Add files via upload

---
+++ PemrogramanKomputer/src/Practice/opp/Squeak.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practice.opp;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Squeak {
+    public void quack() {
+        System.out.println("Squeak");
+    }
+}
